8eaf39dcaabb01134ebb2cb2c92ad3e05344b91c
leon <lsu@movoto.com>
2017-01-12 14:08:22 +0800

    add comments for settings

--- settings.py
+++ settings.py
@@ -2,12 +2,15 @@
 """
 Create a settings_local.py as your local config
 When run startup.py, it will try to load the settings_local.py first
+Example:
+1. for HOME_PATH, if you are running in windows, you can add your home path config in settings_local.py
+2. for run mode, you your local, you may want to run in DEBUG=True, also use the settings_local.py
 """
 from mylogger import mylogger
 
 log = mylogger.get_instance()
 
-HOME_PATH = "/home/"  # linux style path, should change this path value if in windows
+HOME_PATH = "/home/"  
 
 DEBUG = False
 
